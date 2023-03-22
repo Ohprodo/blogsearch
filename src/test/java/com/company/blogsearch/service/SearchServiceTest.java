c0ae4598856ba7f4be7b0a20feed9f5cabca0a51
Ohprodo <ohks9208@naver.com>
2023-03-22 22:30:04 +0900

    build jar파일

--- src/test/java/com/company/blogsearch/service/SearchServiceTest.java
+++ src/test/java/com/company/blogsearch/service/SearchServiceTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static com.company.blogsearch.constant.Const.KAKAO_SORT_ACCRC;
 
